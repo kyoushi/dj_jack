0076a6339f2ae7ba5c3438ea301a7804f6616cfa
mitsu <mitsu@RYZEN7>
2020-04-12 22:19:53 -0400

    base64

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template, jsonify
 import getSong
 import playVLC
 import json
+import base64
 
 from playVLC import Song
 
@@ -14,6 +15,27 @@ stream_song = Song()
 
 # Windows10
 # https://thbdqxnhzjqwpp432sgi2k.us-west.webrelay.io/api/v1/play_music
+
+def encode_base64(message):
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+
+    return base64_message
+
+
+def decode_base64(base64_message):
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+
+    return message
+
+
+@app.route('/base64')
+def index():
+    return render_template('index.html')
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -37,7 +59,7 @@ def play_song():
             response_text = "{'payload':{'google':{'expectUserResponse':true,'richResponse':{'items':[{'simpleResponse':{'textToSpeech':'OK! playing " + \
                             track[0] + " by " + track[1] + "'}}]}}}}"
             print(response_text)
-            
+
     except:
         song = content['queryResult']['parameters']['song'][0].strip('"')
         print(song)
@@ -51,4 +73,27 @@ def play_song():
     return Response(response_text, status=201, mimetype='application/json')
 
 
-app.run(debug=True, port=8080) # run app in debug mode on port 5000
+@app.route('/api/v1/resources/base64', methods=['GET'])
+def api_all():
+    if 'fstring' in request.args:
+        fstring = request.args['fstring']
+    else:
+        return "Error: No fstring field provided. Please specify an fstring."
+        # Create an empty list for our results
+    if 'type' in request.args:
+        type = request.args['type']
+    else:
+        return "Error: No type field provided. Please specify an type."
+        # Create an empty list for our results
+
+    if type.lower() == 'encode':
+        base64_message = encode_base64(fstring)
+        return base64_message
+    elif type.lower() == 'decode':
+        message = decode_base64(fstring)
+        return message
+    else:
+        return "Please specify type 'encode' or 'decode'"
+
+
+app.run(debug=True, port=8080)  # run app in debug mode on port 5000
