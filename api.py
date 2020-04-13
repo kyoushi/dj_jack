e4d32195b38ab5436923423d2544ad4cd299504a
mitsu <mitsu@RYZEN7>
2020-04-12 21:29:36 -0400

    Fix 04122020

--- api.py
+++ api.py
@@ -3,8 +3,17 @@ import getSong
 import playVLC
 import json
 
-app = Flask(__name__)  # create the Flask app
+from playVLC import Song
 
+app = Flask(__name__)  # create the Flask app
+stream_song = Song()
+
+
+# RaspberryPI
+# https://0lt6bbtf0hbp2knmhts63g.webrelay.io/api/v1/play_music
+
+# Windows10
+# https://thbdqxnhzjqwpp432sgi2k.us-west.webrelay.io/api/v1/play_music
 
 @app.route('/', methods=['GET'])
 def home():
@@ -16,42 +25,23 @@ def home():
 def play_song():
     content = request.json
     try:
+        print('LOOK! 1')
         print(json.dumps(content, indent=4))
         content = content['queryResult']['parameters']['action'].strip('"')
-        print(content)
         if content == 'stop':
-            print(content)
-            playVLC.stop_song(playVLC.player)
             response_text = "{'payload':{'google':{'expectUserResponse':false,'richResponse':{'items':[{'simpleResponse':{'textToSpeech':'OK! music stopped'}}]}}}}"
+            stream_song.stop_song()
     except:
         song = content['queryResult']['parameters']['song'][0].strip('"')
         print(song)
         track = getSong.ask_for_song(song)
         stream_url = track[2]
-        player = playVLC.play_song(stream_url)
-        response_text = "{'payload':{'google':{'expectUserResponse':true,'richResponse':{'items':[{'simpleResponse':{'textToSpeech':'OK! playing "+track[0]+" by "+track[1]+"'}}]}}}}"
-
+        stream_song.play_song(stream_url)
+        response_text = "{'payload':{'google':{'expectUserResponse':true,'richResponse':{'items':[{'simpleResponse':{'textToSpeech':'OK! playing " + \
+                        track[0] + " by " + track[1] + "'}}]}}}}"
         print(response_text)
 
-    # song = json.dumps((content['queryResult']['parameters']['song'][0]), indent=4)
-    # print(song)
-    # print(content['queryResult'])
-    # song = content['song']
-
-    # song = song.replace('"', '')
-    # stream_url = getSong.ask_for_song(song)
-    # player = playVLC.play_song(stream_url)
-
     return Response(response_text, status=201, mimetype='application/json')
-
-
-# @app.route('/api/v1/action', methods=['POST'])
-# def player_action():
-#     content = request.json
-# 
-#     playVLC.stop_song(playVLC.player)
-# 
-#     return Response("{'Status':'Success'}", status=201, mimetype='application/json')
 
 
 app.run(debug=True, port=80)  # run app in debug mode on port 5000
